4c53b70e284c0aa62ccb8db261e644af21588521
Chris Wade <swade781977@yahoo.com>
2020-02-22 19:52:12 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,15 +7,15 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  "SELECT projects.title, SUM(pledges.amount) FROM projects INNER JOIN pledges ON projects.id = pledges.project_id ORDER BY title ASC;"
+  "SELECT projects.title, pledges.amount FROM projects INNER JOIN pledges ON projects.id = pledges.project_id ORDER BY title ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT users.name, users.age, pledges.amount FROM users LEFT OUTER JOIN pledges ON users.id = pledges.user_id ORDER BY name ASC;"
+  "SELECT users.name, users.age, SUM(pledges.amount) FROM users LEFT OUTER JOIN pledges ON users.id = pledges.user_id ORDER BY name ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) FROM projects INNER JOIN pledges ON projects.id = pledges.project_id HAVING SUM(pledges.amount) >= projects.funding_goal;"
+  "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
